bebc35f6f0a5fad768227c9d5b775452961d1d3d
berylcl <gakiiberyl@gmail.com>
2023-06-26 20:31:31 +0100

    Implementing coins in this economy

---
+++ src/components/Coin.js
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+import Icon from 'react-icons-kit';
+import { arrowCircleRight } from 'react-icons-kit/fa/arrowCircleRight';
+
+const Coin = ({ name, symbol, icon }) => (
+    <div className="coin" data-testid="coin">
+        <i>
+            <Icon icon={arrowCircleRight} size={20} />
+        </i>
+        <img src={icon} alt={name} />
+        <span>{name.substring(0, 15)}</span>
+        <span>{symbol.substring(0, 15)}</span>
+    </div>
+);
+
+Coin.propTypes = {
+    name: PropTypes.string.isRequired,
+    symbol: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+};
+
+export default Coin;
