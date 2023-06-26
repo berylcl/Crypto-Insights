9554993d577ceeea8c693fe3027f267cdb006343
berylcl <gakiiberyl@gmail.com>
2023-06-26 21:43:34 +0100

    MOdifying coin.js"

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -3,20 +3,20 @@ import Icon from 'react-icons-kit';
 import { arrowCircleRight } from 'react-icons-kit/fa/arrowCircleRight';
 
 const Coin = ({ name, symbol, icon }) => (
-    <div className="coin" data-testid="coin">
-        <i>
-            <Icon icon={arrowCircleRight} size={20} />
-        </i>
-        <img src={icon} alt={name} />
-        <span>{name.substring(0, 15)}</span>
-        <span>{symbol.substring(0, 15)}</span>
-    </div>
+  <div className="coin" data-testid="coin">
+    <i>
+      <Icon icon={arrowCircleRight} size={20} />
+    </i>
+    <img src={icon} alt={name} />
+    <span>{name.substring(0, 15)}</span>
+    <span>{symbol.substring(0, 15)}</span>
+  </div>
 );
 
 Coin.propTypes = {
-    name: PropTypes.string.isRequired,
-    symbol: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  symbol: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
 };
 
 export default Coin;
