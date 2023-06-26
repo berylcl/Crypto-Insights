671b58b3a6382cf25ed836894e9159293886b0c2
berylcl <gakiiberyl@gmail.com>
2023-06-26 21:43:03 +0100

    Add app.js file

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Header from './components/Header';
-
+import Homepage from './components/HomePage';
+import DetailsPage from './components/DetailsPage';
+import store from './redux/store';
 import './App.css';
 
 const App = () => (
-    <Provider store={store}>
-      <div className="App">
-        <Router>
-          <Header />
-          <Routes>
-          </Routes>
-        </Router>
-      </div>
-    </Provider>
+  <Provider store={store}>
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/:coinId" element={<DetailsPage />} />
+        </Routes>
+      </Router>
+    </div>
+  </Provider>
 );
 
 export default App;
