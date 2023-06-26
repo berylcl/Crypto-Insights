66fefeb1e4c226dbfd56ed7b62d4ab3ccee954b6
berylcl <gakiiberyl@gmail.com>
2023-06-26 21:44:05 +0100

    Implementing header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,38 +2,38 @@ import React, { useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const Header = () => {
-    const [searchName, setSearchName] = useState('');
-    useLocation();
-    const navigate = useNavigate();
+  const [searchName, setSearchName] = useState('');
+  useLocation();
+  const navigate = useNavigate();
 
-    const handleSearch = (event) => {
-        event.preventDefault();
-        if (searchName) {
-            const encodedSearchName = encodeURIComponent(searchName);
-            navigate(`/?coinName=${encodedSearchName}`);
-        } else {
-            navigate('/');
-        }
-    };
+  const handleSearch = (event) => {
+    event.preventDefault();
+    if (searchName) {
+      const encodedSearchName = encodeURIComponent(searchName);
+      navigate(`/?coinName=${encodedSearchName}`);
+    } else {
+      navigate('/');
+    }
+  };
 
-    return (
-        <header>
-            <Link to="/">
-                <div className="logo">
-                    <span>CRYPTO CURRENCIES</span>
-                </div>
-            </Link>
-            <form role="form" onSubmit={handleSearch}>
-                <input
-                    type="text"
-                    value={searchName}
-                    onChange={(event) => setSearchName(event.target.value)}
-                    placeholder="Search..."
-                />
-                <button type="submit">Search</button>
-            </form>
-        </header>
-    );
+  return (
+    <header>
+      <Link to="/">
+        <div className="logo">
+          <span>CRYPTO CURRENCIES</span>
+        </div>
+      </Link>
+      <form role="form" onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={searchName}
+          onChange={(event) => setSearchName(event.target.value)}
+          placeholder="Search..."
+        />
+        <button type="submit">Search</button>
+      </form>
+    </header>
+  );
 };
 
 export default Header;
