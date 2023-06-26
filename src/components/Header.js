bd26d5d524203d08920f7a857a3f557b89f5364a
berylcl <gakiiberyl@gmail.com>
2023-06-26 21:59:51 +0100

    EDiting header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,11 @@
-import React, { useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import React from 'react';
 
-const Header = () => {
-  const [searchName, setSearchName] = useState('');
-  useLocation();
-  const navigate = useNavigate();
-
-  const handleSearch = (event) => {
-    event.preventDefault();
-    if (searchName) {
-      const encodedSearchName = encodeURIComponent(searchName);
-      navigate(`/?coinName=${encodedSearchName}`);
-    } else {
-      navigate('/');
-    }
-  };
-
-  return (
-    <header>
-      <Link to="/">
-        <div className="logo">
-          <span>CRYPTO CURRENCIES</span>
-        </div>
-      </Link>
-      <form role="form" onSubmit={handleSearch}>
-        <input
-          type="text"
-          value={searchName}
-          onChange={(event) => setSearchName(event.target.value)}
-          placeholder="Search..."
-        />
-        <button type="submit">Search</button>
-      </form>
-    </header>
-  );
-};
+const Header = () => (
+  <header>
+    <div className="logo">
+      <span>CRYPTO CURRENCIES</span>
+    </div>
+  </header>
+);
 
 export default Header;
