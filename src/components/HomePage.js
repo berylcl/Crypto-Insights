13ef9ea39e802ac92dee92d8f279cb403854fe41
berylcl <gakiiberyl@gmail.com>
2023-06-26 21:48:43 +0100

    Implementing home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,31 +5,31 @@ import Coin from './Coin';
 import { fetchCoins } from '../redux/reducers';
 
 const Homepage = () => {
-    const coinState = useSelector((state) => state.coins);
-    const dispatch = useDispatch();
+  const coinState = useSelector((state) => state.coins);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if (coinState.length === 0) {
-            dispatch(fetchCoins());
-        }
-    }, [coinState.length, dispatch]);
+  useEffect(() => {
+    if (coinState.length === 0) {
+      dispatch(fetchCoins());
+    }
+  }, [coinState.length, dispatch]);
 
-    return (
-        <div className="coin-list-container" data-testid="homepage">
-            <div className="card-container">
-                {coinState.map((coin) => (
-                    <Link key={coin.id} to={`/${coin.id}`}>
-                        <Coin
-                            key={coin.id}
-                            name={coin.name}
-                            symbol={coin.symbol}
-                            icon={coin.icon}
-                        />
-                    </Link>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="coin-list-container" data-testid="homepage">
+      <div className="card-container">
+        {coinState.map((coin) => (
+          <Link key={coin.id} to={`/${coin.id}`}>
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              symbol={coin.symbol}
+              icon={coin.icon}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
